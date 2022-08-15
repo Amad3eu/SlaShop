f147a07c3a55bee58825599b0762ed55b6e3a2c8
Luiz Felipe Warmling Amadeu <luizfelipewarmling@gmail.com>
2022-08-15 13:25:49 +0000

    [FIX]: Alter locale replace

--- utilities/changeNumbersFormatEnToFa.ts
+++ utilities/changeNumbersFormatEnToFa.ts
@@ -1,2 +1,2 @@
-export const changeNumbersFormatEnToPt= (number: number | string) =>
+export const changeNumbersFormatEnToFa= (number: number | string) =>
   number.toString().replace(/\d/g, (index) => "0123456789"[index]);
